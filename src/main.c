dabb19cad16255a43bbdc0d24d514ae342da744f
Flawake <60014996+Flawake@users.noreply.github.com>
2026-02-05 14:18:43 +0100

    Implement wifi connection and hide secrets in .env

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
+#include "net.h"
 #include "esp_log.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
 #include "freertos/FreeRTOS.h"
 
 void app_main() {
-    while (1) {
-        ESP_LOGI("task_app_main", "Hello world");
-        vTaskDelay(1000/portTICK_PERIOD_MS);
-    }
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    wifi_init();
 }
---
+++ src/net.h
@@ -0,0 +1,6 @@
+#ifndef NET_H
+#define NET_H
+
+void wifi_init(void);
+
+#endif
