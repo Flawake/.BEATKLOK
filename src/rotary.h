32b8f4dccb8bc71ae5bdb720922857f085ba3100
Flawake <60014996+Flawake@users.noreply.github.com>
2026-02-24 22:15:56 +0100

    Fix logic bug

--- src/rotary.h
+++ src/rotary.h
@@ -1,11 +1,15 @@
 #ifndef ROTARY_H
 #define ROTARY_H
 
+#include <stdbool.h>
+
 typedef enum {
     ROTARY_NONE = 0,
     ROTARY_CW,
     ROTARY_CCW
 } e_ROTARY_STEP;
+
+const char *rotary_step_to_string(e_ROTARY_STEP step);
 
 void init_rotary_encoder();
 
