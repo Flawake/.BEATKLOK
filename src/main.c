54c501233404bb21138878aeba107806fb006e86
Flawake <60014996+Flawake@users.noreply.github.com>
2026-02-26 20:30:06 +0100

    Implement system mode

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "net.h"
+#include "mode.h"
 #include "ntp.h"
 #include "esp_log.h"
 #include "esp_event.h"
@@ -11,5 +12,6 @@ void app_main() {
 
     wifi_init();
 
-    xTaskCreate(sntp_sync_task, "ntp_monitor_task", 4096, NULL, 5, NULL);
+    xTaskCreate(ntp_monitor_task, "ntp_monitor_task", 4096, NULL, 5, NULL);
+    xTaskCreate(update_mode_task, "update_mode_task", 4096, NULL, 5, NULL);
 }
