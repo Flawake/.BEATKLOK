df43dc21c3d8def8425980751b06697307e9f8ba
Flawake <60014996+Flawake@users.noreply.github.com>
2026-03-02 23:20:48 +0100

    Use interrupts for rotary encoder reading

--- src/rotary.c
+++ src/rotary.c
@@ -1,6 +1,21 @@
+#include <stdint.h>
 #include "rotary.h"
 #include "driver/gpio.h"
 #include "debounce.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "esp_timer.h"
+
+#define BUTTON_ISR_QUEUE_SIZE 50
+
+typedef struct
+{
+    uint32_t time_ms;
+    button_state_t state;
+    gpio_num_t triggered_pin;
+} button_sample_t;
+
+static QueueHandle_t button_queue;
 
 typedef struct {
     gpio_num_t key_pin;
@@ -10,7 +25,8 @@ typedef struct {
 
 typedef struct {
     S_RotaryEncoderConfig config;
-    bool last_s2;
+    button_state_t last_s2;
+    int16_t step_count;
 } S_RotaryEncoder;
 
 S_RotaryEncoder rotary_encoder = {
@@ -19,7 +35,8 @@ S_RotaryEncoder rotary_encoder = {
         .s1_pin = GPIO_NUM_18,
         .s2_pin = GPIO_NUM_5,
     },
-    .last_s2 = true,
+    .last_s2 = RELEASED,
+    .step_count = 0,
 };
 
 button_debouncer_t key_button;
@@ -36,34 +53,6 @@ const char *rotary_step_to_string(e_ROTARY_STEP step)
     }
 }
 
-void set_encoder_pin(gpio_num_t pin_num) {
-    gpio_config_t io_conf = {
-        .pin_bit_mask = 1ULL << pin_num,
-        .mode = GPIO_MODE_INPUT,
-        .pull_up_en = GPIO_PULLUP_ENABLE
-    };
-    gpio_config(&io_conf);
-}
-
-void init_rotary_encoder(void) {
-    set_encoder_pin(rotary_encoder.config.key_pin);
-    set_encoder_pin(rotary_encoder.config.s1_pin);
-    set_encoder_pin(rotary_encoder.config.s2_pin);
-
-    bool s1_state = (bool)gpio_get_level(rotary_encoder.config.s1_pin);
-    bool s2_state = (bool)gpio_get_level(rotary_encoder.config.s2_pin);
-
-    button_debouncer_init(&key_button, RELEASED, DEFAULT_DEBOUNCE_TIME_MS);
-    button_debouncer_init(&sw1_key, s1_state, DEFAULT_DEBOUNCE_TIME_MS);
-    button_debouncer_init(&sw2_key, s2_state, DEFAULT_DEBOUNCE_TIME_MS);
-}
-
-button_state_t read_rotary_button(void) {
-    // Invert button since pull up is ative on the pin
-    bool button_pin_state = (bool)gpio_get_level(rotary_encoder.config.key_pin) == 0;
-    return button_debouncer_update(&key_button, button_pin_state, esp_timer_get_time());
-}
-
 e_ROTARY_STEP rotary_detect_update(button_state_t current_s1, button_state_t current_s2, button_state_t last_s2) {
     if (current_s2 != last_s2 && current_s2 == RELEASED) {
         return (current_s1 == RELEASED) ? ROTARY_CW : ROTARY_CCW;
@@ -71,15 +60,90 @@ e_ROTARY_STEP rotary_detect_update(button_state_t current_s1, button_state_t cur
     return ROTARY_NONE;
 }
 
-e_ROTARY_STEP read_rotary_step(void) {
-    bool current_s1 = (bool)gpio_get_level(rotary_encoder.config.s1_pin);
-    bool current_s2 = (bool)gpio_get_level(rotary_encoder.config.s2_pin);
-    button_state_t s1_state = button_debouncer_update(&s1_state, current_s1, esp_timer_get_time());
-    button_state_t s2_state = button_debouncer_update(&s2_state, current_s2, esp_timer_get_time());
+static void replay_encoder_interrupts() {
+    if (button_queue == NULL) {
+        return;
+    }
 
-    e_ROTARY_STEP step = rotary_detect_update(s1_state, s2_state, rotary_encoder.last_s2);
+    button_sample_t sample;
+    while (xQueueReceive(button_queue, &sample, 0) == pdTRUE) {
+        if (sample.triggered_pin == rotary_encoder.config.key_pin) {
+            (void)button_debouncer_update(&key_button, sample.state, sample.time_ms);
+            continue;
+        }
 
-    rotary_encoder.last_s2 = current_s2;
+        if (sample.triggered_pin == rotary_encoder.config.s1_pin) {
+            (void)button_debouncer_update(&sw1_key, sample.state, sample.time_ms);
+            continue;
+        }
 
-    return step;
+        if (sample.triggered_pin == rotary_encoder.config.s2_pin) {
+            button_state_t current_s1 = sw1_key.stable_state;
+            button_state_t current_s2 = button_debouncer_update(&sw2_key, sample.state, sample.time_ms);
+
+            e_ROTARY_STEP step = rotary_detect_update(current_s1, current_s2, rotary_encoder.last_s2);
+            rotary_encoder.last_s2 = current_s2;
+
+            if (step == ROTARY_CW) {
+                rotary_encoder.step_count++;
+            } else if (step == ROTARY_CCW) {
+                rotary_encoder.step_count--;
+            }
+        }
+    }
+}
+
+static void IRAM_ATTR encoder_isr_handler(void *arg) {
+    gpio_num_t triggered_pin = (gpio_num_t)(uintptr_t)arg;
+    button_sample_t sample = {
+        .time_ms = esp_timer_get_time() / 1000,
+        .state = gpio_get_level(triggered_pin) ? RELEASED : PRESSED,
+        .triggered_pin = triggered_pin
+    };
+
+    xQueueSendFromISR(button_queue, &sample, NULL);
+}
+
+void set_encoder_pin(gpio_num_t pin_num) {
+    gpio_config_t io_conf = {
+        .pin_bit_mask = 1ULL << pin_num,
+        .mode = GPIO_MODE_INPUT,
+        .pull_up_en = GPIO_PULLUP_ENABLE,
+        .intr_type = GPIO_INTR_ANYEDGE,
+    };
+    gpio_config(&io_conf);
+
+    gpio_isr_handler_add(pin_num, encoder_isr_handler, (void *)(uintptr_t)pin_num);
+}
+
+void init_rotary_encoder(void) {
+    button_queue = xQueueCreate(BUTTON_ISR_QUEUE_SIZE, sizeof(button_sample_t));
+
+    gpio_install_isr_service(ESP_INTR_FLAG_IRAM);
+    set_encoder_pin(rotary_encoder.config.key_pin);
+    set_encoder_pin(rotary_encoder.config.s1_pin);
+    set_encoder_pin(rotary_encoder.config.s2_pin);
+
+    button_state_t s1_state = gpio_get_level(rotary_encoder.config.s1_pin) ? RELEASED : PRESSED;
+    button_state_t s2_state = gpio_get_level(rotary_encoder.config.s2_pin) ? RELEASED : PRESSED;
+
+    button_debouncer_init(&key_button, RELEASED, DEFAULT_DEBOUNCE_TIME_MS);
+    button_debouncer_init(&sw1_key, s1_state, DEFAULT_DEBOUNCE_TIME_MS);
+    button_debouncer_init(&sw2_key, s2_state, DEFAULT_DEBOUNCE_TIME_MS);
+}
+
+button_state_t read_rotary_button(void) {
+    replay_encoder_interrupts();
+
+    uint32_t now_ms = esp_timer_get_time() / 1000;
+    button_state_t raw = gpio_get_level(rotary_encoder.config.key_pin) ? RELEASED : PRESSED;
+    return button_debouncer_update(&key_button, raw, now_ms);
+}
+
+int16_t read_rotary_step() {
+    replay_encoder_interrupts();
+
+    int16_t steps = rotary_encoder.step_count;
+    rotary_encoder.step_count = 0;
+    return steps;
 }
--- src/rotary.h
+++ src/rotary.h
@@ -2,6 +2,10 @@
 #define ROTARY_H
 
 #include <stdbool.h>
+#include <stddef.h>
+#include "debounce.h"
+
+#define ROTARY_MAX_STEPS 50
 
 typedef enum {
     ROTARY_NONE = 0,
@@ -13,7 +17,7 @@ const char *rotary_step_to_string(e_ROTARY_STEP step);
 
 void init_rotary_encoder();
 
-bool read_rotary_button();
+button_state_t read_rotary_button(void);
 
-e_ROTARY_STEP read_rotary_step();
+int16_t read_rotary_step(void);
 #endif
