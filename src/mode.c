29df087f9fa0993003754b0bb93c46e871e84a15
Flawake <60014996+Flawake@users.noreply.github.com>
2026-03-03 23:17:34 +0100

    Move motor based on mode

--- src/mode.c
+++ src/mode.c
@@ -7,8 +7,6 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-static const char *TAG = "MODE";
-
 e_SystemMode currentSystemMode = MANUAL;
 
 e_SystemMode update_system_mode(e_SystemMode currentMode) {
@@ -33,27 +31,13 @@ void update_mode_task(void *arg) {
 
     init_rotary_encoder();
 
-    static char rotary_text[5];
-
     while(1) {
         currentSystemMode = update_system_mode(currentSystemMode);
 
-        int16_t step_count = read_rotary_step();
-        if (step_count != 0) {
-            ESP_LOGI(TAG, "Rotary step: %i", step_count);
-
-            uint8_t len = snprintf(rotary_text, sizeof(rotary_text), "%i", step_count);
-
-            uint16_t text_width = len * 8;
-            S_Vector2 pos = {
-                .x = (128 - text_width) / 2,
-                .y = (64 - 8) / 2,
-            };
-
-            render_text(rotary_text, pos, len);
-            
-            buzzer_play_tone(1000, 100);
-            motor_move_relative(90);
+        if (currentSystemMode == MANUAL) {
+            int16_t step_count = read_rotary_step();
+            motor_move_relative(step_count * 4);
+            motor_reset_local_rotation();
         }
 
         vTaskDelay(pdMS_TO_TICKS(100));
